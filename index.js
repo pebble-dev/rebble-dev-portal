a01413864e5a506f85a71851415331e00a83fd39
Willow Systems <mail@willow.systems>
2021-03-26 17:41:20 +0000

    Inital conversion to nodeJS based server. Dumped legacy jekyll

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var app = express();
+var path = require('path');
+
+var htmlPath = __dirname + "/html"
+
+var indexPaths = ["/","/profile"];
+
+app.use("/res", express.static(path.join(htmlPath + "/res")));
+
+app.get(indexPaths, (req, res) => {
+	res.sendFile(path.join(htmlPath + '/index.html'));
+});
+app.get("/login*", (req, res) => {
+	res.sendFile(path.join(htmlPath + '/login.html'));
+});
+app.get("/auth*", (req, res) => {
+	res.sendFile(path.join(htmlPath + '/login-callback.html'));
+});
+app.get("/favicon.ico", (req, res) => {
+	res.sendFile(path.join(htmlPath + "/favicon.ico"))
+});
+
+app.listen(8082, () => {
+	console.log("Started listening on port 8082");
+});
