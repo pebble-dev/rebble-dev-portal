accd4b9ad9b2a8a69ccbf2f042d095ee6a08209f
Willow Systems <mail@willow.systems>
2021-04-06 20:53:09 +0100

    Add config for edit endpoint

--- html/res/js/config.js
+++ html/res/js/config.js
@@ -5,7 +5,8 @@ config = {
     path: {
         aboutme: "/api/v0/users/me/developer",
         appInfo: "/api/v1/apps/id/",
-        submitApp: "/api/v2/submit"
+        submitApp: "/api/v2/submit",
+        editApp: "/api/v2/app/"
     },
     misc: {
         appstoreUrl: "https://apps.rebble.io/en_US/application/"
